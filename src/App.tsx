6df122cdcdc88fae752290ec181683d3882b569b
PC <mirgthebest@gmail.com>
2024-04-03 15:35:55 +0300

    add prod

--- src/App.tsx
+++ src/App.tsx
@@ -22,3 +22,6 @@ function App() {
 }
 
 export default App
+
+// 1. Create a TXT record in your DNS configuration for the following hostname: _github-pages-challenge-randepukperesoh.huntercorptest.co 
+// 2. Use this code for the value of the TXT record: f909e375e544a0dffbad1fb4709bac 3. Wait until your DNS configuration changes. This could take up to 24 hours to propagate.
